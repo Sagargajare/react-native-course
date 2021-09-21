b2c84ab5a4a043558e9130213e4363212058d609
Sagar Gajare <56780589+Sagargajare@users.noreply.github.com>
2021-09-21 21:27:41 +0530

    assignment of diceRoller done

--- diceRoller/App.js
+++ diceRoller/App.js
@@ -4,7 +4,7 @@ import {
   StyleSheet,
   View,
   TouchableOpacity,
-  StatusBar,
+  Pressable,
   Image,
 } from 'react-native';
 import DiceOne from './assets/dice1.png';
@@ -14,17 +14,67 @@ import DiceFour from './assets/dice4.png';
 import DiceFive from './assets/dice5.png';
 import DiceSix from './assets/dice6.png';
 const App = () => {
-  const uri = DiceFive;
+  const [uri, setUri] = useState(DiceOne);
+  const playButtonTapped = () => {
+    let randomNumber = Math.floor(Math.random() * 6) + 1;
+    switch (randomNumber) {
+      case 1:
+        setUri(DiceOne);
+        break;
+      case 2:
+        setUri(DiceTwo);
+        break;
+      case 3:
+        setUri(DiceThree);
+        break;
+      case 4:
+        setUri(DiceFour);
+        break;
+      case 5:
+        setUri(DiceFive);
+        break;
+      case 6:
+        setUri(DiceSix);
+        break;
+
+      default:
+        setUri(DiceOne);
+        break;
+    }
+  };
   return (
     <>
-      <View>
-        <Image source={uri} />
-        <TouchableOpacity>
-          <Text>Play Game</Text>
-        </TouchableOpacity>
+      <View style={styles.container}>
+        <Pressable onPress={playButtonTapped}>
+          <Image style={styles.image} source={uri} />
+        </Pressable>
       </View>
     </>
   );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#222831',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  image: {
+    width: 200,
+    height: 200,
+  },
+  gamePlayButton: {
+    fontSize: 20,
+    marginTop: 30,
+    color: '#F2A365',
+    paddingHorizontal: 40,
+    paddingVertical: 10,
+    borderColor: '#30475E',
+    borderRadius: 5,
+    borderWidth: 3,
+    fontWeight: 'bold',
+  },
+});
